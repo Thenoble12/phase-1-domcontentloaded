1707535cf7355f1a321b5d2349ecb503b5d9d5a9
Elijah Noble <elijahnobledev@gmail.com>
2022-05-05 15:03:19 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    console.log("DOM has been Loaded!");
+    let newText = "This is really cool!"
+    document.getElementById("text").textContent = newText
+});
+
+console.log(
+
+)
